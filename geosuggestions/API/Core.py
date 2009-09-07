72cce70624c7135ad1ee0ceca1d6e2562d9155f3
Aaron Straup Cope <straup@gmail.com>
2009-09-07 11:54:39 -0700

    this is the old old

--- geosuggestions/API/Core.py
+++
@@ -1,9 +0,0 @@
-from APIApp import APIApp
-from geosuggestions import Geosuggestions
-
-class CoreHandler (Geosuggestions, APIApp) :
-
-  def __init__ (self) :
-    Geosuggestions.__init__(self)    
-    APIApp.__init__(self)
-
--- geosuggestions/Frob.py
+++
@@ -1,8 +0,0 @@
-from geosuggestions import Geosuggestions
-
-class FrobHandler (Geosuggestions) :
-
-  def get (self):  
-
-    if not self.do_token_dance() :
-      self.response.out.write('SNFU')
