44cbaba4bce8b207a0d06fdd798a6292ed67ba00
Aaron Straup Cope <straup@gmail.com>
2009-09-22 08:59:26 -0700

    formatting

--- javascript/token.js
+++ javascript/token.js
@@ -21,31 +21,31 @@ function confirm_token_change(id, ctx){
 		html += '</form>';
 	}
 
-			if (ctx == 'chooser'){
+	else if (ctx == 'chooser'){
 
-               			var html = '<div id="token_change_' + id + '">';
-               			html += '<span style="font-size:1.5em;">There\'s just one thing:</span><br /><br />';
-               			html += 'If you\'d like to notify a person of a suggestion you\'ve made by leaving a comment ';
-                		html += 'on their photo, the authentication token ';
-              			html += 'that connects your Flickr account with "suggestify" will need to be upgraded ';
-               			html += 'to have "write" permissions.<br /><br />';
-                  		html += 'All that means is that when you click <q style="color:green;">OK</q> you\'ll be sent to Flickr and asked ';
-                  		html += 'again to allow "suggestify" access to your account, only this time with ';
-                  		html += '"write" permissions.<br /><br />';
-                  		html += 'Once you do, you\'ll be sent back here and you can suggestify like crazy.<br /><br />';
-                		html += '<em>Just remember that even if you have a token with "write" permissions the user whose photos your are ';
-                  		html += 'suggesting locations for may have chosen to disallow comments from being added to their photos.</em>';
-               			html += '</div>';
-               			html += '<form method="GET" action="/chooser">';
-                		html += '<input type="hidden" name="perms" value="write">';
-               			html += '<input type="submit" value="I just want to suggest locations (never mind the comments)" onclick="skip_token_change_comments();return false;"';
-                       		html += ' class="reject_token_change"/>';
-               			html += '<input type="submit" value="OK" class="confirm_token_change" />';
-               			html += '<br clear="all" />';
-               			html += '</form>';
-			}
+		var html = '<div id="token_change_' + id + '">';
+		html += '<span style="font-size:1.5em;">There\'s just one thing:</span><br /><br />';
+		html += 'If you\'d like to notify a person of a suggestion you\'ve made by leaving a comment ';
+		html += 'on their photo, the authentication token ';
+		html += 'that connects your Flickr account with "suggestify" will need to be upgraded ';
+		html += 'to have "write" permissions.<br /><br />';
+		html += 'All that means is that when you click <q style="color:green;">OK</q> you\'ll be sent to Flickr and asked ';
+		html += 'again to allow "suggestify" access to your account, only this time with ';
+		html += '"write" permissions.<br /><br />';
+                html += 'Once you do, you\'ll be sent back here and you can suggestify like crazy.<br /><br />';
+		html += '<em>Just remember that even if you have a token with "write" permissions the user whose photos your are ';
+		html += 'suggesting locations for may have chosen to disallow comments from being added to their photos.</em>';
+		html += '</div>';
+		html += '<form method="GET" action="/chooser">';
+		html += '<input type="hidden" name="perms" value="write">';
+		html += '<input type="submit" value="I just want to suggest locations (never mind the comments)" onclick="skip_token_change_comments();return false;"';
+		html += ' class="reject_token_change"/>';
+		html += '<input type="submit" value="OK" class="confirm_token_change" />';
+		html += '<br clear="all" />';
+		html += '</form>';
+	}
 
-	if (ctx == 'chooser_loggedout'){
+	else if (ctx == 'chooser_loggedout'){
 
 		var html = '<div id="token_change_' + id + '">';
        		html += '<span style="font-size:1.5em;">There\'s just one thing:</span><br /><br />';
@@ -68,13 +68,23 @@ function confirm_token_change(id, ctx){
         	html += '</form>';
 	}
 
+        else {
+            	return;            
+        }
+
 	$("#" + id).html(html);
 }
+
+// user does not want a 'write' token for leaving comment
+// notifications so just issue a redirect
 
 function skip_token_change_comments(){
 	location.href='/chooser';
 	return false;
 }
+
+// user doesn't want a 'write' token for reviewing suggestions
+// so offer them something helpful to do in the meantime
 
 function skip_token_change(id){
 
@@ -92,11 +102,17 @@ function skip_token_change(id){
 	$("#" + id).html(html);
 }
 
+// user has clicked one of the links in the navi bar
+// simply overwrite anything in div#main
+
 function confirm_token_change_navi(ctx){
 	confirm_token_change('main', ctx);
 }
 
-function confirm_token_change_review(ctx){
+// user has clicked on the links from a list in div#main
+// stick confirmation message in that
+
+function confirm_token_change_list(ctx){
 	id = ctx + "_link";
 	confirm_token_change(id, ctx);
 }
