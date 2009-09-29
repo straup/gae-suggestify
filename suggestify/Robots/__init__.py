e82123b05669fc93a688892f42b97a3db702eec7
Aaron Straup Cope <straup@gmail.com>
2009-09-29 08:47:37 -0700

    start fleshing out methods; incomplete

--- suggestify/Robots/__init__.py
+++ suggestify/Robots/__init__.py
@@ -1,5 +1,8 @@
 import suggestify
 import config
+import md5
+
+# TO DO (MAYBE) : nonce and/or timestamp?
 
 class Request (suggestify.Request) :
 
@@ -7,40 +10,60 @@ class Request (suggestify.Request) :
     
     suggestify.Request.__init__(self, min_perms)
 
-    self.uuid = uuid
-    self.config = config.robots[uuid]
+  def ensure_config (self, uuid) :
+
+    if not config.robots.has_key(uuid) :
+      return False
     
+    self.config = config.robots[uuid]
+    self.uuid = uuid
+    
+    return True
+  
   def ensure_required_args (self, required) :
 
     for r in required :
 
       if not self.request.get(r) or self.request.get(r) == '' :
-
-        self.error()
         return False
 
     return True
 
-  def ensure_sig (self) :
-
-    sig = self.request.get('sig')
-
-    if sig == '' :
-      self.error()
+  def ensure_robot_sig (self, sig_args, robot_sig) :
+    
+    if robot_sig == '' :
       return False
-      
-    test = self.generate_sig()
 
-    if sig != test :
-      self.error()
+    robot_args = self.collect_robot_args(sig_args)
+    test_sig = self.generate_robot_sig(robot_args)
+
+    if robot_sig != test_sig :
       return False
   
     return True
 
-  def generate_sig (self) :
+  def collect_robot_args (self, keys) :
 
-    secret = self.config['signing_secret']
+    args = {}
     
-  def error (self) :
-    self.response.error(403)
+    for nm in keys :
+      args[ nm ] = self.request.get(nm)
+
+    return args
+  
+  def generate_robot_sig (self, args) :
+
+    raw = []
     
+    keys = args.keys()
+    keys.sort()
+  
+    for nm in keys :
+      raw.append("%s=%s" % (nm, args[nm]))
+      
+    raw.append("_secret=%s" % self.config['signing_secret'])
+    return md5.new("&".join(raw)).hexdigest()
+
+  def error (self, reason='') :
+    self.assign("error", reason)
+    self.display("robots_error.html")
