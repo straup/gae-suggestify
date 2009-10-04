51418bce358eb24326ee5f32f2e775b2f0503cb2
Aaron Straup Cope <straup@gmail.com>
2009-10-04 10:19:07 -0700

    start wrangling robot api approval

--- javascript/suggestibot.js
+++ javascript/suggestibot.js
@@ -28,10 +28,6 @@ info.aaronland.suggestify.Suggestibot.prototype.approve = function(suggestion_id
 
     var _doThisOnSuccess = function(rsp){
 
-        // TO DO:
-        // a) redirect back to /review
-        // b) remove all the other suggestions for photo id
-        
         var photo_url = rsp.getAttribute('photo_url');
 
         var html = '<div class="approve_ok">';
@@ -63,17 +59,105 @@ info.aaronland.suggestify.Suggestibot.prototype.approve = function(suggestion_id
         var s = $("#whirclick_" + suggestion_id).html(html);
     };
 
+    // 
+
     var perms = $("input[name='geo_perms_" + suggestion_id + "']:checked").val();
     var context = $("input[name='geo_context_" + suggestion_id + "']:checked").val();
 
     var meth_args = {
-        'suggestion_id' : suggestion_id,
         'crumb' : this.args['approve_crumb'],
         'geo_perms' : perms,
         'geo_context' : context,
+        '_s' : this.args['robot_sig'],
+        'photo_id' : this.args['robot_photo_id'],
+        'lat' : this.args['robot_lat'],
+        'lon' : this.args['robot_lon'],
+        'acc' : this.args['robot_acc'],
+        'context' : this.args['robot_context'],        
     };
 
-    this.api.api_call('approve', meth_args, _doThisOnSuccess, _doThisIfNot);
+    //
+
+    var _self = this;
+
+    var req = new XMLHttpRequest();
+    var url = this.args['suggestify_apihost']  + '/robots/' + this.args['robot_uuid'];
+
+    console.log(url)
+
+    var params = new Array();
+
+    for (key in meth_args){
+        params.push(encodeURIComponent(key) + '=' + encodeURIComponent(meth_args[key]));
+    }
+    
+    params = params.join("&");
+            
+    // this.log("call " + url + ' with ' + params);
+
+    req.open('POST', url, true);
+
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.setRequestHeader("Content-length", params.length);
+    req.setRequestHeader("Connection", "close");
+
+    _onReadyState = function(e){
+        if (req.readyState != 4){
+            return;
+        }
+
+        // _self.log("api call (" + method + ") dispatch returned with HTTP status: " + req.status);
+
+        if (req.status != 200){
+            // _self.log("api call failed: " + req.responseText);
+
+            if (doThisIfNot){
+                doThisIfNot();
+            }
+
+            return;
+        }
+
+        var rsp;
+        var stat;
+
+        if (req.responseXML){
+            var doc = req.responseXML;
+            rsp = doc.getElementsByTagName("rsp")[0];
+            stat = rsp.getAttribute("stat")
+        }
+
+        else if (typeof(JSON) == 'object') {
+            var txt = req.responseText;
+            var json = JSON.parse(txt);
+            rsp = json['rsp'];
+            stat = rsp['stat'];
+        }
+        
+        else {
+            // _self.log("uhh...there's neither any XML to parse or a JSON parser to parse with. freaking out...");
+            return;
+        }
+
+        // _self.log("api call (" + method + ") dispatch returned with API status: " + stat);
+
+        if ((stat == 'ok') && (doThisOnSuccess)){
+            doThisOnSuccess(rsp);
+        }
+
+        else if (doThisIfNot){
+            doThisIfNot(rsp);
+        }
+
+        else {
+            // _self.log("no API callbacks defined for method call (" + method + "), oh well...");
+        }
+    };
+
+    req.onreadystatechange = _onReadyState;
+    req.send(params);  
+
+    //
 
     this.showWhirClick(suggestion_id);
     return;    
@@ -81,40 +165,11 @@ info.aaronland.suggestify.Suggestibot.prototype.approve = function(suggestion_id
 
 info.aaronland.suggestify.Suggestibot.prototype.reject = function(suggestion_id){
 
-    var _doThisOnSuccess = function(rsp){
-
-        var html = '<span class="reject_ok">'
-        html += 'OK! That suggestion has been rejected';
-        html += '</span>';
-
-        $("#whirclick_" + suggestion_id).html(html);
-    };
+    var html = '<span class="reject_ok">'
+    html += 'OK! That suggestion has been rejected';
+    html += '</span>';
     
-    var _doThisIfNot = function(rsp){
-
-        if (! rsp){
-            alert("Hrm. Something went wrong calling the API!");
-            return;
-        }
-
-        var err = rsp.getElementsByTagName("error")[0];
-        var msg = err.getAttribute("message");
-
-        var html = '<span class="reject_fail">'
-        html += 'Hrm. Rejection failed with the following error: "' + msg + '"';
-        html += '</span>';
-
-        var s = $("#whirclick_" + suggestion_id).html(html);
-    };
-    
-    var meth_args = {
-        'suggestion_id' : suggestion_id,
-        'crumb' : this.args['reject_crumb']
-    };
-
-    this.api.api_call('reject', meth_args, _doThisOnSuccess, _doThisIfNot);
-
-    this.showWhirClick(suggestion_id);
+    $("#whirclick_" + suggestion_id).html(html);
     return;    
 };
 
