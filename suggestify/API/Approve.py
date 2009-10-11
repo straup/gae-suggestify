5e1b05382324e3ed29c02dd5fe5ddc543a524cd3
Aaron Straup Cope <straup@gmail.com>
2009-10-10 18:28:05 -0700

    always check whether a photo's been geotagged already; hooks for post-approval comment (untested)

--- suggestify/API/Approve.py
+++ suggestify/API/Approve.py
@@ -20,22 +20,26 @@ class ApproveHandler (suggestify.API.Request) :
         
         if not suggestion:
             return 
-    
+
+        # Here's a potential problem: In order to do this properly
+        # we may need to call the Flickr API (6) times and we're
+        # doing all of them synchronously there's always the risk
+        # that one of them will time out. Some possibilities include
+        # doing the machinetag and comment stuff as queued tasks but
+        # that probably puts too many eggs in the app engine basket.
+        
+        # Required:
+        # flickr.photos.getInfo
+        # flickr.photos.geo.setLocation
+
+        # Possible:
+        # flickr.photos.geo.setPerms        
+        # flickr.photos.addTags
+        # flickr.photos.comments.addComment
+        
         #
         # Check to see if the photo has already been geotagged
         #
-
-	# Note: It's no longer clear why this got commented out.
-        # My guess is because it was I was concerend about the
-        # actual approval timing out. There are two things to
-        # consider here: 1) scrumjax-ing the check when a user
-        # hits approve 2) storing a local index of photos that
-        # have been geotgged either by suggestify or during a
-        # separate check 3) all of the above.
-        #
-        # (20090905/asc)
-        
-        """
         
         args = {
             'photo_id' : suggestion.photo_id,
@@ -54,8 +58,6 @@ class ApproveHandler (suggestify.API.Request) :
         
             self.api_error(9, 'Photo has already been geotagged')
             return
-
-        """
         
         #
         # Okay! Geotag the fucking photo!!!
@@ -129,45 +131,63 @@ class ApproveHandler (suggestify.API.Request) :
                 pass
 
         #
-        # geo:suggestedby= machine tag but only if the geo perms are public
+        # geo:suggestedby= machine tag and comment-y reward
+        # comment but only if the geo perms are public
         #
 
-	# This is probably a good candidate to do client-side if the approval
-        # returns OK
-        
         if geoperms == 1 and suggestion.suggestor_nsid != self.user.nsid :
 
-            # NSID is the default
-            
             suggested_by = suggestion.suggestor_nsid
-
-            # But try to get their path alias (since this is immutable)
-            
             suggestor = User.get_user_by_nsid(suggestion.suggestor_nsid)
                     
             if suggestor and suggestor.path_alias :
                 suggested_by = suggestor.path_alias
 
-            # Now tag
-            
             tags = "geo:suggestedby=%s" % suggested_by
         
-            args = {
+            machinetags_args = {
                 'photo_id' : suggestion.photo_id,
                 'tags' : tags,
                 'auth_token' : self.user.token,
                 'check_response' : 1,
                 }
-        
-            rsp = self.api_call('flickr.photos.addTags', args)
 
+            rsp = self.api_call('flickr.photos.addTags', machinetags_args)
+
+            # sudo make me a preference
+            
+            if self.user.nsid == 'brooklyn museum' :
+
+                suggested_date = suggestion.date_create
+                suggestor_name = suggestor.username
+                suggestor_url = 'http://www.flickr.com/photos/%s' % suggestor.nsid
+
+                if suggestor.path_alias != '' :
+                    suggestor_url = 'http://www.flickr.com/photos/%s' % suggestor.path_alias
+                    
+                # Note: don't lookup/display the place name for the WOE ID
+                # until it's possible to do corrections on approval.
+                
+                comment = """On %s, <a href="%s">%s</a> suggested where this photo was taken, and they were right!
+                """ % (suggested_date, suggestor_url, suggestor_name)
+
+                method = 'flickr.photos.comments.addComment'
+            
+                comments_args = {
+                    'photo_id' : photo_id,
+                    'comment_text' : comment,
+                    'auth_token' : self.user.token,
+                }
+                
+                rsp = self.api_call(method, args)
+            
         #
         # Update (possibly do this before setting tags?)
         #
         
         Suggestion.approve_suggestion(suggestion)
         Suggestion.reject_all_pending_suggestions_for_photo(suggestion.photo_id)
-        
+
         #
         # HAPPY
         #
@@ -178,5 +198,8 @@ class ApproveHandler (suggestify.API.Request) :
             photo_owner = self.user.path_alias
             
         photo_url = "http://www.flickr.com/photos/%s/%s" % (photo_owner, suggestion.photo_id)
+
+        #
         
-        return self.api_ok({'photo_url' : photo_url})
+        out = {'photo_url' : photo_url}            
+        return self.api_ok(out)
